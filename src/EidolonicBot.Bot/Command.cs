099b492093a11552cf97e17c335a8e0cae9f9887
Alexander Salamatov <ch1seL@ya.ru>
2023-05-15 11:55:58 +0300

    add IsBotInitCommand attribute field

--- src/EidolonicBot.Bot/Command.cs
+++ src/EidolonicBot.Bot/Command.cs
@@ -4,30 +4,35 @@ public enum Command {
     Unknown,
 
     [Command("/help",
-        Description = "Show this help")]
+        Description = "Show this help",
+        IsBotInitCommand = true)]
     Help,
 
     [Command("/wallet",
         Description = "Wallet address and balance",
-        IsWalletNeeded = true)]
+        IsWalletNeeded = true,
+        IsBotInitCommand = true)]
     Wallet,
 
     [Command("/send",
         Description = "Sends tokens to another telegram user that you reply to",
-        IsWalletNeeded = true)]
+        IsWalletNeeded = true,
+        IsBotInitCommand = true)]
     [CommandArg("amount", "minimum 0.1 or all to send the whole balance")]
     Send,
 
     [Command("/withdrawal",
         Description = "Withdraw tokens to address",
-        IsWalletNeeded = true)]
+        IsWalletNeeded = true,
+        IsBotInitCommand = true)]
     [CommandArg("amount", "minimum 0.1 or all to send the whole balance")]
     [CommandArg("address", "you outer address or address of the recipient", dependsOn: "amount")]
     [CommandArg("memo", "(optional) e.g. `user-id` for exchanges", dependsOn: "address")]
     Withdraw,
 
     [Command("/subscription",
-        Description = "Get or control list of subscriptions")]
+        Description = "Get or control list of subscriptions",
+        IsBotInitCommand = true)]
     [CommandArg("list", "show subscriptions for this chat")]
     [CommandArg("add", "subscribe to transactions")]
     [CommandArg("remove", "unsubscribe from transactions")]
