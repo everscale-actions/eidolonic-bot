a33ebf5dcb141da72f137a4cd1950de2544dbe58
Alexander Salamatov <ch1seL@ya.ru>
2024-08-01 17:47:31 +0300

    try to fix ArgumentOutOfRangeException

--- src/EidolonicBot.Utils/ShortStringFormatProvider.cs
+++ src/EidolonicBot.Utils/ShortStringFormatProvider.cs
@@ -1,23 +1,30 @@
 namespace EidolonicBot;
 
-public class ShortStringFormatProvider : IFormatProvider, ICustomFormatter {
+public class ShortStringFormatProvider : IFormatProvider, ICustomFormatter
+{
     public static readonly IFormatProvider Instance = new ShortStringFormatProvider();
 
-    public string Format(string? format, object? arg, IFormatProvider? formatProvider) {
-        return arg switch {
+    public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+    {
+        return arg switch
+        {
             null => throw new ArgumentNullException(nameof(arg)),
             string str =>
-                format?.Split("..") switch {
+                format?.Split("..") switch
+                {
                     [var startStr, var endStr]
                         when int.TryParse(startStr, out var start)
-                             && int.TryParse(endStr, out var end) => ((FormattableString)$"{str[..start]}..{str[^end..]}").ToString(Instance),
+                             && int.TryParse(endStr, out var end)
+                             && str.Length > start + end + 1
+                        => ((FormattableString)$"{str[..start]}..{str[^end..]}").ToString(Instance),
                     _ => str
                 },
             _ => arg.ToString()!
         };
     }
 
-    public object? GetFormat(Type? formatType) {
+    public object? GetFormat(Type? formatType)
+    {
         return formatType == typeof(ICustomFormatter) ? this : null;
     }
 }
