71ecf9459844dc66ed3584052d1a1fac64a4a626
Alexander Salamatov <ch1seL@ya.ru>
2024-08-07 16:22:10 +0300

    add whale scale

--- src/EidolonicBot.Subscriptions/Events/SubscriptionReceivedConsumers/ChatNotificationSubscriptionReceivedConsumer.cs
+++ src/EidolonicBot.Subscriptions/Events/SubscriptionReceivedConsumers/ChatNotificationSubscriptionReceivedConsumer.cs
@@ -9,6 +9,9 @@ public class ChatNotificationSubscriptionReceivedConsumer(
   ITelegramBotClient bot,
   ILinkFormatter linkFormatter
 ) : IConsumer<SubscriptionReceived>, IMediatorConsumer {
+  private const byte MaxWhileScale = 6;
+  private const int MinWhileScale = 3;
+
   private readonly string[] _alertMessages = [
     "Dude, check this out!",
     "Man, take a look at this!",
@@ -27,6 +30,16 @@ public class ChatNotificationSubscriptionReceivedConsumer(
     "Bro, you won’t believe this!"
   ];
 
+  private readonly IReadOnlyDictionary<decimal, string> whileScale = new Dictionary<decimal, string>() {
+    { 0, "\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b" },
+    { 1_000, "\ud83d\udc33\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b" },
+    { 10_000, "\ud83d\udc33\ud83d\udc33\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b" },
+    { 100_000, "\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b" },
+    { 1_000_000, "\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc0b\ud83d\udc0b\ud83d\udc0b" },
+    { 10_000_000, "\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc0b\ud83d\udc0b" },
+    { 100_000_000, "\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33\ud83d\udc33" }
+  };
+
   public async Task Consume(ConsumeContext<SubscriptionReceived> context) {
     var (transactionId, address, balanceDelta, from, to, balance) = context.Message;
     var cancellationToken = context.CancellationToken;
@@ -40,8 +53,8 @@ public class ChatNotificationSubscriptionReceivedConsumer(
       .ToArrayAsync(cancellationToken);
 
     var links = linkFormatter.GetTransactionLinks(transactionId);
-    var fromString = from is not null ? $"\ud83d\udce4 {linkFormatter.GetAddressLink(from)}\n" : null;
-    var toString = to.Length > 0 ? $"\ud83d\udce5 {string.Join(',', to.Select(t => linkFormatter.GetAddressLink(t)))}\n" : null;
+    var fromString = from is not null ? $"\u2b05\ufe0f {linkFormatter.GetAddressLink(from)}" : null;
+    var toString = to.Length > 0 ? $"\u27a1\ufe0f {string.Join(',', to.Select(t => linkFormatter.GetAddressLink(t)))}" : null;
 
     await Task.WhenAll(
       chatAndThreadIds.Select(
@@ -63,11 +76,24 @@ public class ChatNotificationSubscriptionReceivedConsumer(
     var direction = balanceDelta > 0 ? "\u2795" : "\u2796";
     var message = _alertMessages[Random.Shared.Next(0, _alertMessages.Length - 1)].ToEscapedMarkdownV2();
     return $"\ud83d\udd75\ufe0f {message}\n" +
-           $"\ud83c\udfe0 {addressLink}\n" +
-           fromString +
-           toString +
-           $"\ud83d\udcb0 {balance.ToEvers()}\n" +
-           $"{direction} {Math.Abs(balanceDelta).ToEvers()}\n" +
+           $"\ud83c\udfe0 {addressLink} " + fromString + toString + "\n" +
+           $"{direction} {Math.Abs(balanceDelta).ToEvers()} {GetWhileScale(balanceDelta)}\n" +
+           $"\ud83d\udcb0 {balance.ToEvers()} {GetWhileScale(balance)}\n" +
            string.Join(" \\| ", links);
+  }
+
+  private static string GetWhileScale(decimal amount) {
+    var deltaLength = Math.Abs(amount).ToString("#").Length;
+    var whiles = deltaLength switch {
+      >= MaxWhileScale + MinWhileScale => MaxWhileScale,
+      >= MinWhileScale + 1 => deltaLength - MinWhileScale,
+      >= 0 => 0,
+      _ => throw new ArgumentOutOfRangeException()
+    };
+    return string.Join(
+      string.Empty,
+      Enumerable.Repeat(0, whiles).Select(_ => "\ud83d\udc33")
+        .Concat(Enumerable.Repeat(0, MaxWhileScale - whiles).Select(_ => "\ud83d\udc0b"))
+        .ToArray());
   }
 }
